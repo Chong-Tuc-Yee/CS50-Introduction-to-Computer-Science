13d2c0a65041dc962f129657a480cf5271a466a0
Chong-Tuc-Yee <107826905+Chong-Tuc-Yee@users.noreply.github.com>
2023-01-24 23:52:56 +0800

    Create 8.sql

---
+++ Pset7/songs/8.sql
@@ -0,0 +1,4 @@
+SELECT name FROM songs WHERE name LIKE '%feat.%');
+
+-- Write a SQL query that lists the names of the songs that feature other artists.
+-- Should not assume anything about ID of any particular songs or artists: Your queries should be accurate even if the id of any particular song or person were different.
